64d5815b98ec425967bbf84ba3fbd35055db9071
c0admin <mustafagldren@gmail.com>
2023-01-21 12:31:14 +0300

    Proje dosyası ekle.

---
+++ ex/ex.cpp
@@ -0,0 +1,27 @@
+﻿// 1-1000 arası 5e tam bölünüp 7 ye tam bölünmeye sayıları sayan, toplamlarını hesaplayan
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Turkish");
+    int i;
+    int j=0 , a = 0;
+    cout << "Bu sayılar şunlardır : "<< endl;
+    for (i = 1; i < 1000; i++) {
+
+        if (i%5== 0 && i%7!=0) {
+            j++;
+            a = a + i;
+            
+            cout <<" " << i;
+        } 
+       
+     }
+    cout << endl << endl << "1-1000 arası 5'e bölünüp 7'ye bölünmeyen sayı adeti = " << j << endl;
+    cout << endl<< "1-1000 arası 5'e bölünüp 7'ye bölünmeyen sayı toplamı = " << a << endl;
+
+    system("pause");
+    return 0;
+}
+
